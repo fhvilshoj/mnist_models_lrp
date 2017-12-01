20fd35a288d1aeb18e7cb5d6dead239a40fac9f1
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-12-01 07:30:46 +0100

    Added data feed for models

---
+++ fetch_data.py
@@ -0,0 +1,66 @@
+import numpy as np
+from tensorflow.examples.tutorials.mnist import input_data
+
+from os import makedirs
+from os.path import exists
+
+data_dir = './MNIST_data'
+data_file = 'mnist.npy'
+
+class DataFeed(object):
+    def __init__(self):
+        self.data = fetch_data()
+        self.max = self.data['train_images'].shape[0]
+        self.random_permutation = []
+        self.offset = 0
+
+        self.reset_permutation()
+
+    def reset_permutation(self):
+        self.random_permutation = np.random.permutation(self.max)
+        self.offset = 0
+
+    def next(self, batch_size):
+        start = self.offset
+        self.offset += min(self.offset + batch_size, self.max)
+
+        selection = self.random_permutation[start:self.offset]
+
+        if self.offset == self.max:
+            self.reset_permutation()
+
+        return self.data['train_images'][selection], self.data['train_labels'][selection]
+
+    def validation(self):
+        return self.data['validation_images'], self.data['validation_labels']
+
+    def test(self):
+        return self.data['test_images'], self.data['test_labels']
+
+def fetch_data():
+    if not exists(data_dir):
+        makedirs('%s' % data_dir)
+
+    if not exists('%s/%s' % (data_dir, data_file)):
+        mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
+        # Scale data do be between -1 and 1
+        train_data = mnist.train.images * 2 - 1
+        validation_data = mnist.validation.images * 2 - 1
+        test_data = mnist.test.images * 2 - 1
+
+        mnist_data = {'train_images': train_data,
+                      'train_labels': mnist.train.labels,
+                      'validation_images': validation_data,
+                      'validation_labels': mnist.validation.labels,
+                      'test_images': test_data,
+                      'test_labels': mnist.test.labels}
+
+        np.save('%s/%s' % (data_dir, data_file),
+                mnist_data)
+    else:
+        mnist_data = np.load('%s/%s' % (data_dir, data_file))
+
+    return mnist_data
+
+if __name__ == '__main__':
+    print(fetch_data())
