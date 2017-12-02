e10457e2cc99fa75c7591d36beece7e994b79df1
Frederik Hvilshøj <fhvilshoj@gmail.com>
2017-12-02 09:58:55 +0100

    New weight initializer

--- nns/big_blocks.py
+++ nns/big_blocks.py
@@ -15,7 +15,7 @@ def get_ten_blocks(*args):
 
 def get_model_with_bolcks(blocks, x, y_, is_training, *args):
     keep_prob = tf.cond(is_training,
-                        true_fn=lambda: 0.5,
+                        true_fn=lambda: 0.7,
                         false_fn=lambda: 1.)
 
     output = x
@@ -27,8 +27,12 @@ def get_model_with_bolcks(blocks, x, y_, is_training, *args):
             # X has shape (None, 784)
             with tf.name_scope("conv_{}".format(i)):
                 # Kernel shape (height, width, input_channels, output_channels)
-                K = tf.Variable(tf.truncated_normal((3, 3, 1, 2), stddev=0.1))
-                kb = tf.Variable(tf.constant(0.1, shape=[2, ]))
+                K = tf.get_variable('Kernel_{}'.format(i),
+                                    shape=(3, 3, 1, 2),
+                                    initializer=tf.contrib.layers.variance_scaling_initializer())
+
+                # K = tf.Variable(tf.truncated_normal((3, 3, 1, 2), stddev=0.1))
+                kb = tf.Variable(tf.constant(1., shape=[2, ]))
 
                 conv_out = tf.nn.conv2d(output, K, [1, 1, 1, 1], 'SAME')
                 conv_out = tf.nn.bias_add(conv_out, kb)
@@ -40,60 +44,63 @@ def get_model_with_bolcks(blocks, x, y_, is_training, *args):
                     is_training=is_training,
                 )
 
-                # Shape (None, 28, 28, 2)
-                conv_out = tf.nn.relu(conv_out)
+            # Shape (None, 28, 28, 2)
+            conv_out = tf.nn.relu(conv_out)
 
             with tf.name_scope("max_pool_{}".format(i)):
                 max_out = tf.nn.max_pool(conv_out, [1, 2, 2, 1], [1, 2, 2, 1], 'SAME')
 
-                # Shape (None, 784)
-                max_out = tf.reshape(max_out, (-1, 14, 14 * 2))
+            # Shape (None, 784)
+            max_out = tf.reshape(max_out, (-1, 14, 14 * 2))
 
             with tf.name_scope("lstm_{}".format(i)):
                 lstm_units = 64
-                with tf.variable_scope("lstm_{}".format(i)):
-                    # Create lstm layer
-                    lstm = tf.contrib.rnn.LSTMCell(lstm_units, forget_bias=0.)
-                    # Put it into Multi RNN Cell
-                    lstm = tf.contrib.rnn.MultiRNNCell([lstm])
-                    # Let dynamic rnn setup the control flow (making while loops and stuff)
-                    # lstm_output shape: (None, 28, 5)
-                    lstm_output, _ = tf.nn.dynamic_rnn(lstm, max_out, dtype=tf.float32)
+            with tf.variable_scope("lstm_{}".format(i)):
+                # Create lstm layer
+                lstm = tf.contrib.rnn.LSTMCell(lstm_units, forget_bias=0.)
+            # Put it into Multi RNN Cell
+            lstm = tf.contrib.rnn.MultiRNNCell([lstm])
+            # Let dynamic rnn setup the control flow (making while loops and stuff)
+            # lstm_output shape: (None, 28, 5)
+            lstm_output, _ = tf.nn.dynamic_rnn(lstm, max_out, dtype=tf.float32)
 
-                lstm_output_reshaped = tf.reshape(lstm_output, (-1, 14 * lstm_units))
+            lstm_output_reshaped = tf.reshape(lstm_output, (-1, 14 * lstm_units))
 
             with tf.name_scope("linear_{}".format(i)):
-                W1 = tf.Variable(tf.truncated_normal((14 * lstm_units, 784), stddev=0.1),
-                                 trainable=True)
+                W1 = tf.get_variable('linear_weight_{}'.format(i),
+                                    shape=(14 * lstm_units, 784),
+                                    initializer=tf.contrib.layers.variance_scaling_initializer())
                 b1 = tf.Variable(tf.constant(0.1, shape=(784,), dtype=tf.float32))
 
                 lin_activation = tf.matmul(lstm_output_reshaped, W1) + b1
 
-                output = tf.nn.relu(lin_activation)
+            output = tf.nn.relu(lin_activation)
 
-                # linear_out = tf.nn.dropout(linear_out, keep_prob)
+            # linear_out = tf.nn.dropout(linear_out, keep_prob)
 
-    output = tf.nn.dropout(output, keep_prob)
+            output = tf.nn.dropout(output, keep_prob)
 
-    with tf.name_scope("linear_out"):
-        W2 = tf.Variable(tf.truncated_normal((784, 10), stddev=0.1),
-                         trainable=True)
+            with tf.name_scope("linear_out"):
+                W2 = tf.Variable(tf.truncated_normal((784, 10), stddev=0.1),
+                                 trainable=True)
         b2 = tf.Variable(tf.constant(0.1, shape=(10,), dtype=tf.float32))
 
         linear_out = tf.matmul(output, W2) + b2
 
-    y = linear_out
+        y = linear_out
 
-    # The fully connected layer
-    # Loss function
-    # Note that tf.nn.softmax_cross_entropy_with_logits internally applies the softmax on the
-    # model's unnormalized model prediction and sums across all classes, and tf.reduce_mean
-    # takes the average over these sums
-    cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
+        # The fully connected layer
+        # Loss function
+        # Note that tf.nn.softmax_cross_entropy_with_logits internally applies the softmax on the
+        # model's unnormalized model prediction and sums across all classes, and tf.reduce_mean
+        # takes the average over these sums
+        cross_entropy = tf.reduce_mean(
 
-    # Training
-    train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
-    # train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
+            tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
-    return tf.nn.softmax(linear_out), train_step
+
+# Training
+train_step = tf.train.AdamOptimizer(8e-5).minimize(cross_entropy)
+# train_step = tf.train.GradientDescentOptimizer(0.001).minimize(cross_entropy)
+
+return tf.nn.softmax(linear_out), train_step
