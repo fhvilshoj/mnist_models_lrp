d6d821e6c5913a95f03e4f8c4e16162868bc7a4e
Frederik Hvilshøj <fhvilshoj@gmail.com>
2018-01-04 11:07:51 +0100

    Dropout for convolution

--- nns/convolution.py
+++ nns/convolution.py
@@ -19,6 +19,8 @@ def get_convolutional_model(x, y_, is_training):
         # Shape (None, 784)
         conv_out = tf.reshape(conv_out, (-1, 784*2))
 
+    conv_out = tf.nn.dropout(conv_out, keep_prob=0.5)
+        
     with tf.name_scope("linear"):
         W = tf.Variable(tf.truncated_normal((784*2, 10), stddev=0.1),
                         trainable=True)
@@ -34,9 +36,7 @@ def get_convolutional_model(x, y_, is_training):
     # model's unnormalized model prediction and sums across all classes, and tf.reduce_mean
     # takes the average over these sums
     cross_entropy = tf.reduce_mean(
-        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y)) \
-                    + 0.005 * tf.nn.l2_loss(K) \
-                    + 0.001 * tf.nn.l2_loss(W)
+        tf.nn.softmax_cross_entropy_with_logits(labels=y_, logits=y))
 
     # Training
     train_step = tf.train.AdamOptimizer().minimize(cross_entropy)
